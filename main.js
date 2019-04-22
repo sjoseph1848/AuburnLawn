851ad97765d053319c04257182beffc407c66796
Samuel <sjoseph1848@gmail.com>
2019-04-21 22:35:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+  //initialize the weather class 
+const weather = new Weather(75088);
+
+
+var currentTemp = weather.getWeather();
+
+
+
---
+++ weather.js
@@ -0,0 +1,28 @@
+  class Weather{
+    constructor(zip) {
+      this.apiKey = config.apiKey;
+      this.zip = zip;
+    }
+    
+    //Fetch weather from API 
+      getWeather() {
+      let jsondata;
+      const response = fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${this.zip}&units=imperial&appid=${this.apiKey}`)
+      .then(function(resp) { return resp.json() }) // Convert data to json
+      .then(function(data) {
+        drawWeather(data);
+        
+      })
+      return jsondata;   
+    }
+  
+    //Change weather location 
+    changeLocation(zip){
+      this.zip = zip;
+    }
+
+  }
+  function drawWeather( d ) {  
+    var fahrenheit = d.main.temp; 
+    document.getElementById('w-humidity').innerHTML = fahrenheit;
+  }
